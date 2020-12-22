4e1fd3bbeb2583acf8c6755bcef08ef1cc7e3320
mattyocode <matthewoliver@live.co.uk>
2020-12-22 17:18:01 +0000

    Added test take checks call args to wiki API

--- tests/test_console.py
+++ tests/test_console.py
@@ -11,6 +11,7 @@ from hypermodern_python import console
 def runner():
     return click.testing.CliRunner()
 
+
 @pytest.fixture
 def mock_requests_get(mocker):
     mock = mocker.patch("requests.get")
@@ -20,14 +21,32 @@ def mock_requests_get(mocker):
     }
     return mock
 
+
 def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
+
+
+def test_main_prints_title(runner, mock_requests_get):
+    result = runner.invoke(console.main)
     assert "Garth's Banquet" in result.output
+
+
+def test_main_invokes_requests_get(runner, mock_requests_get):
+    runner.invoke(console.main)
+    assert mock_requests_get.called
+
+
+def test_main_uses_en_wikipedia_org(runner, mock_requests_get):
+    runner.invoke(console.main)
+    args, _ = mock_requests_get.call_args
+    assert "en.wikipedia.org" in args[0]
+
 
 @patch('requests.get')
 def test_error_messgae(mock_get, runner):
     mock_get.side_effect = RequestException('HTTPError!')
     result = runner.invoke(console.main)
+    assert result.exit_code == 1
     assert result.output == 'Error: HTTPError!\n'
     
