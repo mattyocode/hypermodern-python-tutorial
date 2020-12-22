7767decab9e3c25a3300bff549920b54b4a08a1f
mattyocode <matthewoliver@live.co.uk>
2020-12-22 16:40:24 +0000

    Added noxfile.py

---
+++ README.md
@@ -0,0 +1,3 @@
+# Working through Hypermodern Python tutorial/walk through
+
+Found at: https://cjolowicz.github.io/posts/hypermodern-python-01-setup/
--- tests/test_console.py
+++ tests/test_console.py
@@ -15,18 +15,6 @@ def test_main_succeeds(runner):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
-# Method to be used to replace requests.get
-# def mock_requests_get(*args, **kwargs):
-#     class MockResponse:
-#         def __init__(self, json_data, status_code):
-#             self.json_data = json_data
-#             self.status_code = status_code
-
-#         def raise_for_status(self):
-#             return RequestException(Mock(status=404), 'not found')
-
-#     return MockResponse(None, 404)
-
 @patch('requests.get')
 def test_error_messgae(mock_get, runner):
     mock_get.side_effect = RequestException('HTTPError!')
