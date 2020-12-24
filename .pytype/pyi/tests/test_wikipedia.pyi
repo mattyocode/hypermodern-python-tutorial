13f351d0b35d9d67f32285429d74ed38bc4d09f5
mattyocode <matthewoliver@live.co.uk>
2020-12-24 13:20:46 +0000

    test

---
+++ .pytype/pyi/tests/test_wikipedia.pyi
@@ -0,0 +1,5 @@
+# (generated with --quick)
+
+wikipedia: module
+
+def test_random_page_uses_given_langugae(mock_requests_get) -> None: ...
--- tests/test_console.py
+++ tests/test_console.py
@@ -1,60 +1,70 @@
-import click.testing
+from unittest.mock import Mock
+
 import click
+import click.testing
+from click.testing import CliRunner
 import pytest
+from pytest_mock import MockFixture
 from requests import RequestException
 
 from hypermodern_python import console
 
 
 @pytest.fixture
-def runner():
+def runner() -> CliRunner:
     return click.testing.CliRunner()
 
 
 @pytest.fixture
-def mock_wikipedia_random_page(mocker):
+def mock_wikipedia_random_page(mocker: MockFixture) -> Mock:
     return mocker.patch("hypermodern_python.wikipedia.random_page")
 
 
 @pytest.mark.e2e
-def test_main_succeeds_in_production_env(runner):
+def test_main_succeeds_in_production_env(runner: CliRunner) -> None:
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
 
-def test_main_succeeds(runner, mock_requests_get):
+def test_main_succeeds(runner: CliRunner, mock_requests_get: Mock) -> None:
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
 
-def test_main_prints_title(runner, mock_requests_get):
+def test_main_prints_title(runner: CliRunner, mock_requests_get: Mock) -> None:
     result = runner.invoke(console.main)
     assert "Garth's Banquet" in result.output
 
 
-def test_main_invokes_requests_get(runner, mock_requests_get):
+def test_main_invokes_requests_get(runner: CliRunner, mock_requests_get: Mock) -> None:
     runner.invoke(console.main)
     assert mock_requests_get.called
 
 
-def test_main_uses_en_wikipedia_org(runner, mock_requests_get):
+def test_main_uses_en_wikipedia_org(runner: CliRunner, mock_requests_get: Mock) -> None:
     runner.invoke(console.main)
     args, _ = mock_requests_get.call_args
     assert "en.wikipedia.org" in args[0]
 
 
-def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
+def test_main_uses_specified_language(
+    runner: CliRunner, mock_wikipedia_random_page: Mock
+) -> None:
     runner.invoke(console.main, ["--language=pl"])
     mock_wikipedia_random_page.assert_called_with(language="pl")
 
 
-def test_main_fails_on_request_error(runner, mock_requests_get):
+def test_main_fails_on_request_error(
+    runner: CliRunner, mock_requests_get: Mock
+) -> None:
     mock_requests_get.side_effect = Exception("Problemo")
     result = runner.invoke(console.main)
     assert result.exit_code == 1
 
 
-def test_main_prints_error_message_on_request_error(runner, mock_requests_get):
+def test_main_prints_error_message_on_request_error(
+    runner: CliRunner, mock_requests_get: Mock
+) -> None:
     mock_requests_get.side_effect = RequestException
     result = runner.invoke(console.main)
     assert "Error" in result.output
