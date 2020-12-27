9a4521d2400a7b093043400e3b041d2529cc2287
mattyocode <matthewoliver@live.co.uk>
2020-12-27 19:27:44 +0000

    Sphinx creating reference for documentation

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+"""Sphinx configuration."""
+project = "hypermodern-tutorial"
+author = "Matt Oliver"
+copyright = f"2020, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+]
