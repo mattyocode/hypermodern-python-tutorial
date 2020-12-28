5332900fe28e2986260458a87be4671a67ef1d57
mattyocode <matthewoliver@live.co.uk>
2020-12-28 12:20:42 +0000

    Seperated coverage -E toml and codecov installs to seperate statements

--- noxfile.py
+++ noxfile.py
@@ -129,6 +129,7 @@ def docs(session: Session) -> None:
 @nox.session(python="3.8")
 def coverage(session: Session) -> None:
     """Upload coverage data."""
-    install_with_constraints(session, "codecov", "coverage -E toml")
+    install_with_constraints(session, "coverage -E toml")
+    install_with_constraints(session, "codecov")
     session.run("coverage", "xml", "--fail-under=0")
     session.run("codecov", *session.posargs)
